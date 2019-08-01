e6c5a1cce869e92f2a183439d541d7e7c4814f9b
= <=>
2019-08-01 11:31:18 +0700

    add file

---
+++ If-else.js
@@ -0,0 +1,11 @@
+function yes_no() {
+    let rep=document.getElementById('reply').value;
+    if(rep=='ECMAScript')
+    {
+        alert('Right');
+    }
+else
+    {
+        alert('Not ECMAScript');
+    }
+}
